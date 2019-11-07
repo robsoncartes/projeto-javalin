ffb19b5ea45838ac4fe475cf30e56964a7d8156b
Maria Oliveira <maria.oliveira@znc.com.br>
2019-11-07 14:12:04 -0300

    Adiciona requistos ao projeto
    Pasta copiada do primeiro repositório

---
+++ requisitos/a-zerinho-o-um/resolucao_zerinho.py
@@ -0,0 +1,23 @@
+def get_vencedor(index):
+    if index == 0:
+        return "A"
+    elif index == 1:
+        return "B"
+    elif index == 2:
+        return "C"
+
+numeros_texto = input("Entre com os numeros escolhidos")
+
+numeros = numeros_texto.split(" ")
+
+n_0 = numeros.count("0")
+n_1 = numeros.count("1")
+
+if n_0 == 1:
+    print (get_vencedor(numeros.index("0")))
+elif n_1 == 1:
+    print (get_vencedor(numeros.index("1")))
+else:
+    print ("*")
+
+        
