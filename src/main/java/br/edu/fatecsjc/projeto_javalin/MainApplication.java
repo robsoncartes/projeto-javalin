244b02679945242d39204d82d885431ff4c93021
Robson Sousa <robsoncartes@outlook.com>
2019-11-17 02:44:23 -0300

    Pequenas alterações no arquivo 'pom.xml'.

--- src/main/java/br/edu/fatecsjc/projeto_javalin/MainApplication.java
+++ src/main/java/br/edu/fatecsjc/projeto_javalin/MainApplication.java
@@ -3,15 +3,12 @@ package br.edu.fatecsjc.projeto_javalin;
 import io.javalin.Javalin;
 
 public class MainApplication {
-	
-	private static String helloWorld = "Hello World";
 
-	public static void main(String[] args) {
-		
-		Javalin app = Javalin.create().start(7001);
-		app.get("/hello", ctx -> ctx.result(helloWorld));
-		
+    private static String helloWorld = "Hello World";
 
-	}
+    public static void main(String[] args) {
 
+        Javalin app = Javalin.create().start(7001);
+        app.get("/hello", ctx -> ctx.result(helloWorld));
+    }
 }
