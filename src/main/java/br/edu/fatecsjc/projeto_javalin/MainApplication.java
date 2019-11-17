23094244901aa14d96a6900404488508e290076a
Robson Sousa <robsoncartes@outlook.com>
2019-11-17 00:59:41 -0300

    Atualização do Projeto. Resolvido: Problema compatibilidade de nomenclaturas de diretíros e packages do projeto entre Eclipse e Intellij.

---
+++ src/main/java/br/edu/fatecsjc/projeto_javalin/MainApplication.java
@@ -0,0 +1,17 @@
+package br.edu.fatecsjc.projeto_javalin;
+
+import io.javalin.Javalin;
+
+public class MainApplication {
+	
+	private static String helloWorld = "Hello World";
+
+	public static void main(String[] args) {
+		
+		Javalin app = Javalin.create().start(7001);
+		app.get("/hello", ctx -> ctx.result(helloWorld));
+		
+
+	}
+
+}
