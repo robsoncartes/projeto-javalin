9644312eaf054693b8ea3ae17fb721bd4bff1ab3
Robson Sousa <robsoncartes@outlook.com>
2019-11-17 21:04:59 -0300

    Alteração da classe Problema: utilizando o Lombok para substituir métodos getters and setters, métodos construtores, método toString e Equals e HashCode.

--- src/main/java/br/edu/fatecsjc/projeto_javalin/models/Problema.java
+++ src/main/java/br/edu/fatecsjc/projeto_javalin/models/Problema.java
@@ -1,53 +1,21 @@
 package br.edu.fatecsjc.projeto_javalin.models;
 
-import java.util.Objects;
+import lombok.*;
 
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@EqualsAndHashCode
+@ToString
 public class Problema {
 
+    @Getter
+    @Setter
     private String filename;
+
+    @Getter
+    @Setter
     private String problem;
+
+    @Getter
+    @Setter
     private String sourcecode;
-
-    public Problema(String filename, String problem, String sourcecode) {
-        this.filename = filename;
-        this.problem = problem;
-        this.sourcecode = sourcecode;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
-    public String getProblem() {
-        return problem;
-    }
-
-    public void setProblem(String problem) {
-        this.problem = problem;
-    }
-
-    public String getSourcecode() {
-        return sourcecode;
-    }
-
-    public void setSourcecode(String sourcecode) {
-        this.sourcecode = sourcecode;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Problema)) return false;
-        Problema problema = (Problema) o;
-        return filename.equals(problema.filename);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(filename);
-    }
 }
