aa323f22a6a1eccb0d2d88ecc9f81ea13ce14461
Robson Sousa <robsoncartes@outlook.com>
2019-11-21 07:33:08 -0300

    Remoção das Anotações providas pelo Lombok na classe Problema. Utilização dos métodos getters, setters, equals, hashCode, toString e construtores da classe de forma tradcional.

--- src/main/java/br/edu/fatecsjc/projeto_javalin/models/Problema.java
+++ src/main/java/br/edu/fatecsjc/projeto_javalin/models/Problema.java
@@ -1,22 +1,71 @@
 package br.edu.fatecsjc.projeto_javalin.models;
 
-import lombok.*;
+import java.util.Objects;
 
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
-@EqualsAndHashCode
-@ToString
 public class Problema {
 
-    @Getter
-    @Setter
+    private Integer id;
     private String filename;
-
-    @Getter
-    @Setter
     private String problem;
-
-    @Getter
-    @Setter
     private String sourcecode;
 
+    public Problema() {
+    }
+
+    public Problema(Integer id, String filename, String problem, String sourcecode) {
+        this.id = id;
+        this.filename = filename;
+        this.problem = problem;
+        this.sourcecode = sourcecode;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getProblem() {
+        return problem;
+    }
+
+    public void setProblem(String problem) {
+        this.problem = problem;
+    }
+
+    public String getSourcecode() {
+        return sourcecode;
+    }
+
+    public void setSourcecode(String sourcecode) {
+        this.sourcecode = sourcecode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Problema)) return false;
+        Problema problema = (Problema) o;
+        return id.equals(problema.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + "\tFilename: " + filename + "\tProblem: " + problem + "\tSourcecode: " + sourcecode;
+    }
 }
