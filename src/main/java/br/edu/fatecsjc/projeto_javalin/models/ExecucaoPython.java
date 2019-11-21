4a5b2c3de51e6466af7e1338c245c62bc79812e7
MARIA CLARA DE OLIVEIRA <mariaclara1985@MARIAs-MacBook-Pro.local>
2019-11-21 03:33:01 -0300

    inclusao de apsta de teste, e class Execucao python

---
+++ src/main/java/br/edu/fatecsjc/projeto_javalin/models/ExecucaoPython.java
@@ -0,0 +1,45 @@
+package br.edu.fatecsjc.projeto_javalin.models;
+
+import java.io.IOException;
+
+public class ExecucaoPython {
+
+    private Process p;
+
+    private String entrada_teste;
+
+    private String saida_teste;
+
+    public ExecucaoPython(String entrada_teste, String saida_teste) throws IOException {
+        setEntrada_teste(entrada_teste);
+        setSaida_teste(saida_teste);
+        p = Runtime.getRuntime().exec(this.getCommand(this.entrada_teste, this.saida_teste));
+
+    }
+
+    public String getEntrada_teste() {
+        return entrada_teste;
+    }
+
+    public void setEntrada_teste(String entrada_teste) {
+        this.entrada_teste = entrada_teste;
+    }
+
+    public String getSaida_teste() {
+        return saida_teste;
+    }
+
+    public void setSaida_teste(String saida_teste) {
+        this.saida_teste = saida_teste;
+    }
+
+    public String getCommand(String entrada_teste, String saida_teste){
+        return "python < " + entrada_teste + "> " + saida_teste;
+    }
+
+
+//    private String run = "python < " + entrada_teste + "> " + saida_teste;
+
+//    private System.out.println(String run);
+
+}
---
+++ src/main/testes/br/edu/fatecsjc/projeto_javalin/models/ExecucaoPythonTest.java
@@ -0,0 +1,7 @@
+package br.edu.fatecsjc.projeto_javalin.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExecucaoPythonTest {
+
+}
